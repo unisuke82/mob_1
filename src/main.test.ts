a9af351005f7b66a768f830ad66054467ac4dff8
uni82 <mayu1368@gmail.com>
2022-07-08 19:59:36 +0900

    Complete todo list Add first test

--- src/main.test.ts
+++ src/main.test.ts
@@ -5,3 +5,10 @@ describe("test", () => {
     expect(true).toBe(true);
   });
 });
+
+
+describe("受け取った引数をstring型に変換して返却する。", () => {
+  it("1を受け取って'1'を返却する", () => {
+      expect(numberToString(1)).toBe('1');
+  });
+})
